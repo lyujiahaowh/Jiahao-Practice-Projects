45a2d50f5251cbcf6b02ee5d7d5a372a50d2fea0
Lyu, Jiahao <jiahao.lyu@gatech.edu>
2023-02-17 01:19:50 +0800

    0217 py4e learning

---
+++ py4e/course 2.py
@@ -0,0 +1,31 @@
+# 2.1 Experssions
+# Fixed values such as numbers,letters,strings are called "cosntants" because they do not change
+# Numeric constants are as you expect
+# String constants are use single quotes ('') or double quotes ("")
+
+# Reserved words - you cannot use reserved words as variable names / identifiers
+# false, class, finally, is, return, None, continue, for, lambda, try, True, def, from, nonlocal, while, and, del, global, not, with, as, elif, if, or, yield, assert, else, import, pass, break, except, in, raise
+
+# in python , = means <-
+# Variables - A variable is a nemed place in the memory where a programmer can store data and later retrieve the data using the variable "name"
+# Programmers choose names for variables that are meaningful to the program
+# You can change the content of a variable at any time
+# Python variable name rules - 1. must start with a letter or underscore 2. must consist of letters, numbers, and underscores 3. case sensitive
+
+# Sentences - A sentence in a program is a unit of code that the Python interpreter can execute
+# Sentences have structure and syntax
+# A sentence in Python is composed of expressions and statements
+# Sentences must end with a new line character
+# Sentences or Lines - x = 2 is an assignment statement / x = x + 2 is an assignment/assignment with experssion statement / print(x) is an expression/print statement
+# organge is variable, white is operator, blue is constant, yellow is function
+# Mnenomics - A memory aid, usually involving a pattern of letters, ideas, or associations, that assists in remembering something. It is only good for humans, not computers.
+# Phython does not understand plural nouns, so it is better to use singular nouns for variables
+# However , it is helpful to use plural nouns for lists when something has more than one value
+# Python does not understand past tense, so it is better to use present tense for variables
+
+# Assignment Statements
+# We assign a value to a variable using an assignment statement (=)
+# An assignment statement consists of an experession on the right-hand side and a variable to store result
+
+# 2.2 Expressions
+# 
